f8b6c5aa148b86c0f7651cb1b064705e26157191
Graciella Dharmawan <g.dharmawan@student.maastrichtuniversity.nl>
2021-11-10 18:17:33 +0100

    create volcano coin

---
+++ week2/Day2/VolcanoCoin.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract VolcanoCoin {
+    uint supply = 10000;
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        if (msg.sender == owner) {
+            _;
+        }
+    }
+
+    function getSupply() public view returns (uint) {
+        return supply;
+    }
+
+    function increaseSupply() public onlyOwner {
+        supply += 1000;
+        emit Supply_increased(supply);
+    }
+
+    event Supply_increased(uint);
+}
