c69c92a5a8af012a2d01267fc780b0f06f1ce244
"gridgies" <"graciella@t-online.de">
2021-11-24 16:44:16 +0100

    solve abstract error

--- week3/Day1/HW5 VolcanoCoin.sol
+++ week3/Day1/HW5 VolcanoCoin.sol
@@ -4,9 +4,18 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 pragma solidity ^0.8.0;
 
-contract VolcanoCoin is ERC20("VolcanoCoin", "VOL"), Ownable {
+contract VolcanoCoin is ERC20("VolcanoCoin", "VLC"), Ownable {
     
     uint initialSupply = 10000;
+    
+    struct Payment {
+        uint amount;
+        address recipient;
+    }
+
+    mapping (address => Payment[]) public payments;
+    
+    event supplyIncreased(uint256);
     
     constructor() {
         _mint(msg.sender, initialSupply);
@@ -14,28 +23,16 @@ contract VolcanoCoin is ERC20("VolcanoCoin", "VOL"), Ownable {
     
     function mintToken(uint _quantity) public onlyOwner {
         _mint(msg.sender, _quantity);
-        emit Supply_increased(_quantity);
+        emit supplyIncreased(_quantity);
     }
     
-    uint supply;
+    uint256 supply;
 
-    function getSupply() public view returns (uint) {
-        return supply;
+    function increaseSupply(uint256 _quantity) public onlyOwner {
+        _mint(msg.sender, _quantity);
+        emit supplyIncreased(_quantity);
     }
 
-    function increaseSupply() public onlyOwner {
-        supply += 1000;
-        emit Supply_increased(supply);
-    }
-
-    event Supply_increased(uint);
-
-    struct Payment {
-        uint amount;
-        address recipient;
-    }
-
-    mapping (address => Payment[]) public payments;
     
     function transfer(address _recipient, uint _amount) public virtual override returns (bool) {
         _transfer(msg.sender, _recipient, _amount);
